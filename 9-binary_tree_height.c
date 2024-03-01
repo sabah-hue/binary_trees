ed688249842c9d627b8d923c6cd8f3d1fb87e8ea
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-01 03:52:38 +0200

    height tree debug

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,6 +15,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 	if (tree->left)
 		left_height = binary_tree_height(tree->left);
 	else
