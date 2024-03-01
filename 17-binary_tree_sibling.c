6ecb04bfd6a0bb0eaa9725ea98ec776aa76b09b6
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-01 05:56:29 +0200

    sibling issue

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * binary_tree_sibling -  finds the sibling of a node
@@ -11,5 +12,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-
+	if (node == node->parent->right)
+		/*check if the given node is right child*/
+		/*if true return siblling which is left child*/
+		return (node->parent->left);
+	return (node->parent->right);
 }
