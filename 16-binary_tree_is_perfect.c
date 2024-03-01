ceb01040d7e6c079f959143c5498a983ca94ce42
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-01 07:13:22 +0200

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,8 +9,14 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int check_left = 0, check_right = 0;
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left == NULL && tree->right)
 		return (0);
+	if (tree->right == NULL && tree->right)
+		return (0);
+	check_left = binary_tree_is_perfect(tree->left);
+	check_right = binary_tree_is_perfect(tree->right);
+	return (check_left && check_right);
 }
